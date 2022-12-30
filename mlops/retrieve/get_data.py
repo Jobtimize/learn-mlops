232097dc644f4f8e8c4a5f2094450dd6e3e9b56a
Job Almekinders <job.almekinders@ndwcloud.nu>
2022-12-30 13:23:59 +0100

    refactor import

--- mlops/retrieve/get_data.py
+++ mlops/retrieve/get_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.datasets import fetch_california_housing
+from sklearn import datasets
 
 from mlops import constants
 
@@ -26,7 +26,7 @@ def get_california_housing_data() -> pd.DataFrame:
 
 def _get_data() -> pd.DataFrame:
     """Get data and transform into pandas dataframe."""
-    raw_data = fetch_california_housing()
+    raw_data = datasets.fetch_california_housing()
     formatted_data = pd.DataFrame(raw_data.data, columns=raw_data.feature_names)
     formatted_data[raw_data.target_names[0]] = raw_data.target
     formatted_data = formatted_data.rename(columns=COLUMN_NAMES_MAPPING)
