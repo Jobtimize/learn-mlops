1893979c8f4bb70bd8ed1e607688d2c9e3ecef18
Job Almekinders <job.almekinders@ndwcloud.nu>
2022-12-30 13:26:45 +0100

    add predict request to app

--- mlops/app.py
+++ mlops/app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from mlops.modeling import predict
 
 app = FastAPI()
 
@@ -6,3 +8,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/predict_request")
+async def predict_request(request: Request):
+    return predict.predict_request(request)
--- mlops/modeling/predict.py
+++ mlops/modeling/predict.py
@@ -4,7 +4,7 @@ from fastapi import Request
 from mlops.modeling import util
 
 
-async def predict(request: Request) -> float:
+async def predict_request(request: Request) -> float:
     model = util.load_model()
     data = await request.json()
     data = pd.DataFrame([data])
