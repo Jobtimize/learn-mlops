97a3b876d77c378e83457affb519c6ab4e6b745c
Job Almekinders <job.almekinders@ndwcloud.nu>
2022-12-30 13:43:23 +0100

    move predict to app.py

--- mlops/app.py
+++ mlops/app.py
@@ -1,6 +1,8 @@
+import pandas as pd
+import uvicorn
 from fastapi import FastAPI, Request
 
-from mlops.modeling import predict
+from mlops.modeling import util
 
 app = FastAPI()
 
@@ -12,4 +14,11 @@ async def root():
 
 @app.post("/predict_request")
 async def predict_request(request: Request):
-    return predict.predict_request(request)
+    model = util.load_model()
+    data = await request.json()
+    data = pd.DataFrame([data])
+    return model.predict(data)[0]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
